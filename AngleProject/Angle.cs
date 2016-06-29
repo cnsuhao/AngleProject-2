0c40e78cebcc8a7ee95786ecd4de88258376618a
viorel.budeci <viorel.budeci@MDDSK40050>
2016-06-29 15:56:19 +0300

    First version of Program, v.1.0

---
+++ AngleProject/Angle.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngleProject
+{
+    class Angle
+    {
+        public int _degree;
+        public int _minute;
+        public int _second;
+
+        public Angle() { }
+        public Angle(int degree, int minute, int second)
+        {
+            _degree = degree;
+            _minute = minute;
+            _second = second;
+        }
+
+        public static Angle NormilizeAngle(Angle myAngle)
+        {
+            int restSecond = 0;
+            int restMinute = 0;
+            int restDegree = 0;
+
+            if (myAngle._second > 60)
+            {
+                restSecond = myAngle._second / 60;
+                myAngle._second %= 60;
+            }
+
+            if (myAngle._minute > 60)
+            {
+                restMinute = myAngle._minute / 60;
+                myAngle._minute %= 60;
+                myAngle._minute += restSecond;
+            }
+
+            myAngle._degree += restMinute;
+
+            if (myAngle._degree > 360)
+            {
+                restDegree = myAngle._degree % 360;
+                myAngle._degree = restDegree;
+            }
+
+            return myAngle;
+
+        }
+
+        public static Angle NormilizeNegativeAngle(Angle myAngle)
+        {
+            int restSecond = 0;
+            int restMinute = 0;
+            int restDegree = 0;
+
+            if (myAngle._second < -60)
+            {
+                restSecond = myAngle._second / 60;
+                myAngle._second %= 60;
+            }
+
+            if (myAngle._minute < -60)
+            {
+                restMinute = myAngle._minute / 60;
+                myAngle._minute %= 60;
+                myAngle._minute += restSecond;
+            }
+
+            myAngle._degree += restMinute;
+
+            if (myAngle._degree < -360)
+            {
+                restDegree = myAngle._degree % 360;
+                myAngle._degree = restDegree;
+            }
+
+            return myAngle;
+
+        }
+        public static Angle NegativeToPositive(Angle myAngle)
+        {
+           if (myAngle._degree < 0 && myAngle._minute < 0 && myAngle._second < 0)
+           {
+                Angle.NormilizeNegativeAngle(myAngle);
+                myAngle._degree = 360 + myAngle._degree;
+                myAngle._minute = 60 + myAngle._minute;
+                myAngle._second = 60 + myAngle._second;
+
+            }
+
+            return myAngle;
+        }
+        public static int ToSeconds(Angle myAngle)
+        {
+            return (myAngle._degree * 3600) + (myAngle._minute * 60) + myAngle._second;
+        }
+        public static Angle Compliment(Angle myAngle)
+        {
+            if (myAngle._degree > 0 && myAngle._minute > 0 && myAngle._second > 0)
+            {
+                Angle.NormilizeAngle(myAngle);
+                myAngle._degree = myAngle._degree - 360;
+                myAngle._minute = myAngle._minute - 60;
+                myAngle._second = myAngle._second - 60;
+
+            }
+
+            Angle temp = new Angle();
+            temp._second = Math.Abs(myAngle._second);
+            temp._minute = Math.Abs(myAngle._minute);
+            temp._degree = Math.Abs(myAngle._degree);
+
+
+
+            return temp;
+        }
+        public static Angle operator+(Angle lhs, Angle rhs)
+        {   
+            if (lhs._degree < 0 && lhs._minute < 0 && lhs._second < 0)
+            {
+                Angle.NegativeToPositive(lhs);
+            }
+
+            if (rhs._degree < 0 && rhs._minute < 0 && rhs._second < 0)
+            {
+                Angle.NegativeToPositive(rhs);
+            }
+
+            int totalSecond = lhs._second + rhs._second;
+            int totalMinute = lhs._minute + rhs._minute;
+            int totalDegree = lhs._degree + rhs._degree;
+
+            Angle result = new Angle(totalDegree, totalMinute, totalSecond);
+            result = NormilizeAngle(result);
+
+            return result;
+        }
+
+        public static Angle operator-(Angle lhs, Angle rhs)
+        {
+
+            Angle temp = new Angle();
+            temp = Angle.Compliment(rhs);
+
+            int totalSecond =  temp._second + lhs._second;
+            int totalMinute = temp._minute + lhs._minute;
+            int totalDegree =  temp._degree + lhs._degree;
+
+
+            Angle result = new Angle(totalDegree, totalMinute, totalSecond);
+            Angle r =  Angle.Compliment(result);
+            return r;
+        }
+
+        public static bool operator==(Angle lhs, Angle rhs)
+        {
+            int lhsSeconds = Angle.ToSeconds(lhs);
+            int rhsSeconds = Angle.ToSeconds(rhs);
+
+            if (lhsSeconds == rhsSeconds)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Angle lhs, Angle rhs)
+        {
+            int lhsSeconds = Angle.ToSeconds(lhs);
+            int rhsSeconds = Angle.ToSeconds(rhs);
+
+            if (lhsSeconds != rhsSeconds)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <(Angle lhs, Angle rhs)
+        {
+            int lhsSeconds = Angle.ToSeconds(lhs);
+            int rhsSeconds = Angle.ToSeconds(rhs);
+
+            if (lhsSeconds < rhsSeconds)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator >(Angle lhs, Angle rhs)
+        {
+            int lhsSeconds = Angle.ToSeconds(lhs);
+            int rhsSeconds = Angle.ToSeconds(rhs);
+
+            if (lhsSeconds > rhsSeconds)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <=(Angle lhs, Angle rhs)
+        {
+            int lhsSeconds = Angle.ToSeconds(lhs);
+            int rhsSeconds = Angle.ToSeconds(rhs);
+
+            if (lhsSeconds <= rhsSeconds)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator >=(Angle lhs, Angle rhs)
+        {
+            int lhsSeconds = Angle.ToSeconds(lhs);
+            int rhsSeconds = Angle.ToSeconds(rhs);
+
+            if (lhsSeconds >= rhsSeconds)
+                return true;
+            else
+                return false;
+        }
+
+       
+
+        public override string ToString()
+        {
+            return _degree + " " + _minute + "\'" + _second + "\"";
+        }
+
+
+    }
+}
--- AngleProject/Program.cs
+++ AngleProject/Program.cs
@@ -10,6 +10,89 @@ namespace AngleProject
     {
         static void Main(string[] args)
         {
+            Angle a = new Angle(3, 36, 53);
+            Angle b = new Angle(4, 27, 45);
+            Angle aa = new Angle(3, 36, 53);
+            Angle bb = new Angle(4, 27, 45);
+            
+
+
+          
+            Angle a1 = new Angle(44, 27, 45);
+            Angle a2 = new Angle(44, 27, 45);
+            Angle a3 = new Angle(3, 45, 3);
+            Angle a4 = new Angle(34, 7, 55);
+            Angle a5 = new Angle(-34, -7, -55);
+            Angle a5temp = new Angle(-34, -7, -55);
+
+            Console.WriteLine("a = " + a.ToString());
+            Console.WriteLine("b = " + b.ToString());
+            Console.WriteLine("a1 = " + a1.ToString());
+            Console.WriteLine("a2 = " + a2.ToString());
+            Console.WriteLine("a3 = " + a3.ToString());
+            Console.WriteLine("a4 = " + a4.ToString());
+            Console.WriteLine("a5 = " + a5.ToString());
+
+            Console.WriteLine("\n\nOperatii:" );
+            Angle c = new Angle();
+            c = a + b;
+            Angle d = new Angle();
+            d = a - b;
+            Angle e = new Angle();
+            e = bb - aa;
+
+            Angle f = new Angle();
+            f = a3 + a5;
+
+            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " +  c.ToString());
+            Console.WriteLine(a.ToString() + " - " + b.ToString() + " = " + d.ToString());
+            Console.WriteLine(b.ToString() + " - " + a.ToString() + " = " + e.ToString());
+            Console.WriteLine(a3.ToString() + " + " + a5temp.ToString() + " = " + f.ToString());
+
+            if (a1 == a2)
+                Console.WriteLine("a1 = a2");
+            else
+                Console.WriteLine("a1 != a2");
+
+            if (a1 != a3)
+                Console.WriteLine("a1 != a3");
+            else
+                Console.WriteLine("a1 == a3");
+
+            if (a3 >= a4)
+                Console.WriteLine("a3 >= a4");
+            else
+                Console.WriteLine("a3 <= a4");
+
+            
+            if (a4 <= a3)
+                Console.WriteLine("a4 <= a3");
+            else
+                Console.WriteLine("a4 >= a3");
+
+            
+            if (a3 < a4)
+                Console.WriteLine("a3 < a4");
+            else
+                Console.WriteLine("a3 > a4");
+
+            
+            if (a3 > a4)
+                Console.WriteLine("a3 > a4");
+            else
+                Console.WriteLine("a3 < a4");
+
+
+            Angle longAngle = new Angle(123, 345, 567);
+            Console.WriteLine("\nConvert: " + longAngle.ToString());
+            Console.WriteLine("Result: " + Angle.NormilizeAngle(longAngle));
+
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
